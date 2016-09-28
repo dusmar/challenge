839beb60aef7ce17523688186cc491f471894bff
dmarusca <dusanmaruscak@yahoo.co.uk>
2016-09-28 18:50:24 +0200

    initial

---
+++ cchallenge/src/main/java/com/challenges/cchallenge/App.java
@@ -0,0 +1,13 @@
+package com.challenges.cchallenge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
